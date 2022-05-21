6c242e1d29b7cfca3b25b1ea4f74c87cf4e2ef35
Rodrigo Flores de Aquino <rodrigo.aquino@squadra.com.br>
2022-05-20 22:07:56 -0300

    commit initial

---
+++ RecycloPortal/Controllers/PontuacaoController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using RecicloPortal.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RecicloPortal.Controllers
+{
+    public class PontuacaoController : Controller
+    {
+        private readonly ILogger<PontuacaoController> _logger;
+
+        public PontuacaoController(ILogger<PontuacaoController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ RecycloPortal/Controllers/ResgateController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using RecicloPortal.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RecicloPortal.Controllers
+{
+    public class ResgateController : Controller
+    {
+        private readonly ILogger<ResgateController> _logger;
+
+        public ResgateController(ILogger<ResgateController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
